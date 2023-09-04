4d9ab270b1750ca51462e5374a021cbb3aebbb22
Gurm <gurmolsofly12@gmail.com>
2023-09-03 19:16:36 -0600

    Confirmation Page

---
+++ src/components/ConfirmationPage.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import styles from "./styles/ConfirmationPage.module.css";
+import OrderConfirmation from "./OrderConfirmation";
+
+function ConfirmationPage() {
+  const { id } = useParams();
+  const [order, setOrder] = useState(null);
+
+  const fetchOrder = async (orderId) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/orders/${orderId}`
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const data = await response.json();
+      setOrder(data);
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrder(id);
+  }, [id]);
+
+  return (
+    <div className={styles["confirmation-page"]}>
+      {order ? <OrderConfirmation order={order} /> : <p>Loading...</p>}
+    </div>
+  );
+}
+
+export default ConfirmationPage;
--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -7,7 +7,7 @@ function OrderModal({ order, setOrderModal }) {
   const [address, setAddress] = useState("");
 
   const placeOrder = async () => {
-    const response = await fetch("/api/orders", {
+    const response = await fetch("http://localhost:3000/api/orders", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"

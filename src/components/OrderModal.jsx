8f49e9ece5e29be8862b0ba8c8cd128f7c5d0464
Gurm <gurmolsofly12@gmail.com>
2023-09-03 19:53:57 -0600

    fixing localhost

--- src/components/OrderModal.jsx
+++ src/components/OrderModal.jsx
@@ -25,7 +25,7 @@ function OrderModal({ order, setOrderModal }) {
   };
 
   const placeOrder = async () => {
-    const response = await fetch("http://localhost:3001/api/orders", {
+    const response = await fetch("http://localhost:3000/api/orders", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
